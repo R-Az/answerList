e81ffb31b9a3d202677df04222c3ed02630a2300
raz <raztechtech@gmail.com>
2020-10-05 01:45:42 +0900

    初回作成分 index.htmlとcss、ｊｓを作成。

---
+++ js/examType.js
@@ -0,0 +1,10 @@
+export const examType = {
+  alphabet: {
+    label: "アルファベット",
+    choices: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+  },
+  number: {
+    label: "数字",
+    choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+  },
+};
---
+++ js/index.js
@@ -0,0 +1,66 @@
+import { examType } from "./examType.js";
+
+window.onload = () => {
+  const examTypeSelect = document.getElementById("examType");
+  let optionStr = "";
+  Object.keys(examType).forEach((key) => {
+    optionStr += `<option value="${key}">${examType[key].label}</option>\n`;
+  });
+
+  examTypeSelect.innerHTML = optionStr;
+};
+
+document.getElementById("generateExamArea").addEventListener("click", () => {
+  const examArea = document.getElementById("examArea");
+  alert(examArea.innerHTML);
+  if (examArea.innerHTML != "") {
+    const deleteBool = confirm("現在のデータは削除されます。\n宜しいですか？");
+    if (deleteBool) {
+      examArea.innerHTML = "";
+    } else {
+      alert("生成を中断しました。");
+      return;
+    }
+  }
+
+  const selectedExamType = document.getElementById("examType").value;
+  const selectedAnswerNumStr = document.getElementById("answerNum").value;
+  const selectedExamNumStr = document.getElementById("examNum").value;
+
+  const selectedAnswerNum = numValidator(selectedAnswerNumStr, "選択肢数", 10);
+  const selectedExamNum = numValidator(selectedExamNumStr, "問題数", 100);
+
+  const choises = examType[selectedExamType].choices;
+  let optionsList = "";
+
+  for (let index = 0; index < selectedAnswerNum; index++) {
+    optionsList += `<option value="${choises[index]}">${
+      choises[index]
+    }</option>\n`;
+  }
+
+  for (let index = 1; index <= selectedExamNum; index++) {
+    const answerList =
+      `<div><select id="exam${index}" class="inputArea">\n${optionsList}</select></div>`;
+    const examHTML =
+      `<div class="dis_flex"><span>${index}</span>\n${answerList}\n${answerList}\n</div>`;
+    examArea.innerHTML += examHTML;
+  }
+
+  alert("生成完了！");
+});
+
+function numValidator(num, errorTag, maxRange) {
+  num = parseInt(num, 10);
+
+  if (isNaN(num)) {
+    alert(`${errorTag}は半角数字を入力してください`);
+    throw new Error();
+  }
+  if (num < maxRange && 0 > num) {
+    alert(`${errorTag}は1〜${maxRange}まで`);
+    throw new Error();
+  }
+
+  return num;
+}
